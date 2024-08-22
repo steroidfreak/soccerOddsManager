364ee2c50471a02d95239fd506cf1405ea3de30f
Desmond Chua <chuatianboon@gmail.com>
2024-08-23 02:57:54 +0800

    updated with team and logo

--- script.js
+++ script.js
@@ -16,20 +16,22 @@ var requestOptions = {
 // console.log(data.response[0].league.id);
 
 async function search() {
-  const response = await axios.get("https://v3.football.api-sports.io/leagues", {
+  const response = await axios.get("https://v3.football.api-sports.io/fixtures?league=39&season=2024&from=2024-08-23&to=2024-08-30", {
       
       headers: {"x-apisports-key": "95782220272b5bde20c7d5b97070ec44"},
   })
+  console.log(response.data);
   return response.data;
 }
 
 document.addEventListener("DOMContentLoaded", async function(){
+  const data = await search();
 
-  let data = await search();
+  let output = document.querySelector("#soccer");
+  output.innerHTML = ''; // Clear the output element before appending new data
 
   for(let x of data.response){
-
-    console.log(x.league.id,x.league.name);
-    
+    console.log(x.teams.home.name, x.teams.away.name);
+    output.innerHTML += `<h3><img src="${x.teams.home.logo}" style="width: 100%; height: auto; max-width: 50px;">${x.teams.home.name} vs <img src="${x.teams.away.logo}" style="width: 100%; height: auto; max-width: 50px;">${x.teams.away.name}</h3><br>`;
   }
-})
+});
